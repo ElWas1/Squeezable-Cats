27c460231247c5cc09968d0381dd55090558f8d1
ElWas1 <tinoxdwtf@gmail.com>
2023-07-17 13:32:53 -0300

    Linked JSON file with HTML document, improvement in both design and theme

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 const catPic = document.getElementById('squeezable-cat')
+const dropdownBarCats = document.getElementById('animal')
+const newPic = document.getElementById('new-pic')
 
 function showCatNames(array) {
     let pic = '';
     array.forEach(element => {
-        pic = `<select>
-        <option value='${element.name}'></option></select>`
+        pic += `<option value='${element.name}'>${element.name}</option>`
     });
     dropdownBarCats.innerHTML = pic
 }
@@ -12,12 +13,12 @@ function showCatNames(array) {
 let catsArray
 
 async function obtainData() {
-    data = await fetch('./cats_list.json')
-    .then(data => data.json())
-    .then(data => {
-        return data
-    })
-    catsArray = data.cats
+    data = await fetch('scripts/cats_list.json')
+        .then(data => data.json())
+        .then(data => {
+            return data
+        })
+    catsArray = data.surprise
     return data
 }
 
